59b3ece4f6dfa9ceda7e908438605e5b22e8678f
Kuga Kugaraja <contact@kuga.ca>
2019-11-06 16:38:03 -0500

    added context and api call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
+import { Provider } from './context';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-    <Fragment>
-    <Navbar />
-    <div className="container">
-    <Switch>
-      <Route exact path="/" component={Index} />
-    </Switch>
-    </div>
-    </Fragment>
-    </Router>
-
+    <Provider>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Index} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import Spinner from '../layout/Spinner';
 
 class Tracks extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Tracks</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          console.log(value);
+
+          const { track_list } = value;
+
+          if (track_list === undefined || track_list.length === 0) {
+            return <Spinner></Spinner>;
+          } else {
+            return <h1>Tracks Loaded</h1>;
+          }
+        }}
+      </Consumer>
+    );
+  }
 }
 
-export default Tracks
+export default Tracks;
